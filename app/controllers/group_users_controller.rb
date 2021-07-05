8a71b2a34a8f52bc315f9d9efdc37576b65fba93
223yu <223yuta.jp@gmail.com>
2021-07-05 16:32:32 +0000

    Update group member with ajax

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,14 +1,14 @@
 class GroupUsersController < ApplicationController
 
   def create
-    group = Group.find(params[:group_id])
-    current_user.join(group)
-    redirect_to request.referrer || root_path
+    @group = Group.find(params[:group_id])
+    current_user.join(@group)
+    @members = @group.users
   end
 
   def destroy
-    group = Group.find(params[:id])
-    current_user.leave_group(group)
-    redirect_to request.referrer || root_path
+    @group = Group.find(params[:id])
+    current_user.leave_group(@group)
+    @members = @group.users
   end
 end
